b880d0bec496bcfc1f370b26f179d5e91d8e9006
JurajCasar <casar440@gmail.com>
2025-02-01 13:14:20 +0100

    final Main edits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    private static ArrayList<Block> blockChain = new ArrayList<>();
+    private static int difficulty = 5;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Miner miner = new Miner(difficulty);
+
+        //Create first transaction
+        String transactionOne = "Peter pays Michael 5 C";
+        //Miner starts mining based on transaction
+        Block firstBlock = miner.mineBlock("0", transactionOne);
+        //Add block to blockchain
+        blockChain.add(firstBlock);
+
+        //Create first transaction
+        String transactionTwo = "Lucy pays Ronda 7 C";
+        //Miner starts mining based on transaction
+        Block secondBlock = miner.mineBlock(firstBlock.getHash(), transactionTwo);
+        //Add block to blockchain
+        blockChain.add(secondBlock);
+
+        //Create first transaction
+        String transactionThree = "Marta pays Jane 9 C";
+        //Miner starts mining based on transaction
+        Block thirdBlock = miner.mineBlock(secondBlock.getHash(), transactionThree);
+        //Add block to blockchain
+        blockChain.add(thirdBlock);
     }
 }
--- src/Miner.java
+++ src/Miner.java
@@ -2,9 +2,9 @@ public class Miner {
     private int difficulty;
     private String target;
 
-    public Miner(int difficulty, String targer) {
+    public Miner(int difficulty) {
         this.difficulty = difficulty;
-        this.target = new String(new char[targer.length()]).replace('\0', '0');
+        this.target = new String(new char[difficulty]).replace('\0', '0');
     }
 
     Block mineBlock(String previousHash, String transaction){
@@ -15,7 +15,7 @@ public class Miner {
             nonce++;
             hash = HashCreator.calculateHash(previousHash,transaction,nonce);
         }
-        System.out.println("Block was mined successfully at " + nonce + "attempt." + "Hash: " + hash);
+        System.out.println("Block was mined successfully at " + nonce + " attempt." + " Hash: " + hash);
         System.out.println();
 
         return new Block(transaction, previousHash, hash,nonce);
